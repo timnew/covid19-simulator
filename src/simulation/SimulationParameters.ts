4696a1aca4349beec250a32b683c2798de3f1162
Tim <timnew.wti@gmail.com>
2020-03-24 01:04:51 +1100

    Introduce fatality rate

--- src/simulation/SimulationParameters.ts
+++ src/simulation/SimulationParameters.ts
@@ -3,21 +3,24 @@ import { Rectangle } from 'pixi.js'
 export default class SimulationParameters {
   readonly screenHeight: number
   readonly screenWidth: number
-  readonly halfScreenHeight: number
-  readonly halfScreenWidth: number
 
   readonly personRadius: number
-  readonly population: number = 100
+  readonly population: number
+  readonly maxInitialSpeed: number
+  readonly wallBounce: boolean
 
-  readonly maxInitialSpeed: number = 50
-
-  readonly wallBounce: boolean = false
+  readonly fatalityRate: number
 
   constructor(readonly screen: Rectangle) {
     this.screenWidth = screen.width
     this.screenHeight = screen.height
-    this.halfScreenHeight = this.screenHeight / 2
-    this.halfScreenWidth = this.screenWidth / 2
+
     this.personRadius = 5
+    this.population = 100
+
+    this.maxInitialSpeed = 50
+    this.wallBounce = false
+
+    this.fatalityRate = 0.05
   }
 }
